c64768f913390f6657b772298e1dd91d4d9b95ea
http-snehal <snehalkushwah24@gmail.com>
2026-03-03 04:10:56 +0530

    change in structure

---
+++ airbnb/routes/hostRoute.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const express = require("express");
+
+const rootDir = require("../utils/utiils");
+
+
+const hostRoute = express.Router();
+
+
+hostRoute.get('/add-home', (req, res, next) => {
+
+  res.sendFile(path.join(rootDir, 'views/addHome.html'));
+
+});
+   
+
+
+hostRoute.post('/add-home', (req, res, next) => {
+  console.log('Form submitted successfully', req.body);
+  res.sendFile(path.join(rootDir, 'views/homeAdded.html'));
+});
+
+module.exports = hostRoute;
